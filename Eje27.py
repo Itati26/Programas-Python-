14a7c3c1607876af70143ba1529a0b214b817450
Itati26 <ita26rey04@gmail.com>
2025-08-17 20:52:17 -0600

    Create Eje27.py

---
+++ Eje27.py
@@ -0,0 +1,6 @@
+edad = int(input("Por favor, ingresa tu edad: "))
+
+if edad >= 8:
+    print("¡Puedes subir al juego!")
+else:
+    print("Lo siento, no cumples con la edad mínima para subir al juego.")
